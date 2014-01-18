55d1f8bc121cb3737cc4a73e04f1d4b5475a5db1
Vincent Huang <vincom2@gmail.com>
2014-01-18 03:48:22 -0500

    wow very chat much lookup

--- server/server.js
+++ server/server.js
@@ -1,30 +1,39 @@
 var express = require('express');
-var fs = require("fs");
+// var fs = require("fs");
 var file = "test.db";
-var exists = fs.existsSync(file);
-var sql = require('sql');
-var sqlite3 = require('sqlite3');
+// var exists = fs.existsSync(file);
+// var sql = require('sql'); //help this doesn't work I am sad
+var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database(file);
 var app = express();
 
-var chatlogs = sql.define({
-	name: 'chats',
-	columns: [from, to, msg, time]
-});
+// var chatlogs = sql.define({
+// 	name: 'chats',
+// 	columns: ['fromID', 'toID', 'msg', 'time']
+// });
 
 app.get('/get_chats', function(req,res) {
-	var query_string = chatlogs
-						.select(chatlogs.msg, chatlogs.time)
-						.from(chatlogs)
-						.where(user.from.equals(req.query.from))
-						.toQuery();
+	// var bad_query_string = chatlogs
+	// 					.select(chatlogs.msg, chatlogs.time)
+	// 					.from(chatlogs)
+	// 					.where(chatlogs.fromID.equals(req.query.from))
+	// 					.toQuery();
+	// console.log(bad_query_string.text);
+	// var query = db.prepare("select * from chats where fromID = 1");
+	var query_string = "select * from chats where fromID = " + req.query.from;
 
 	db.serialize(function() {
-		if(!exists) {
-			db.run("create table chats (from TEXT, to TEXT, msg TEXT, time DATETIME DEFAULT CURRENT_TIMESTAMP");
-		}
-		var query = db.prepare(query_string);
-	})
+		// if(!exists) {
+		// 	db.run("create table chats (fromID NUMERIC, toID NUMERIC, msg TEXT, time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL)");
+		// }
+
+		db.all(query_string, function(err, rows) {
+			if(err) {
+				console.log(err);
+			}
+			res.send(rows);
+		});
+	});
 });
 
 app.get('/', function(req, res){
