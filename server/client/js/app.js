2c25f37dbd324b3c1ea853565023ee713050edde
Arthur Lee <arthur@arthurlee.me>
2014-01-18 11:00:49 -0500

    Added rel="nojs" option for links

--- server/client/js/app.js
+++ server/client/js/app.js
@@ -1,7 +1,7 @@
 // Click handler, intercepts all links
 $(document).on('click', "a[href^='/']", function(event) {
   var href = $(event.currentTarget).attr('href');
-  var passThrough = href.indexOf('http://') >= 0;
+  var passThrough = href.indexOf('http://') >= 0 || $(event.currentTarget).attr('rel') == 'nojs';
 
   if (!passThrough && !event.altKey && !event.ctrlKey && !event.metaKey && !event.shiftKey) {
     event.preventDefault();
