30f40810375cb8a86a0a95fa5d5b590d2294ead0
Vincent Huang <vincom2@gmail.com>
2014-01-18 06:36:42 -0500

    o wow you can login with facebook and it creates the appropriate user database entries (I think)

--- server/fblogin.js
+++ server/fblogin.js
@@ -1,12 +1,41 @@
 var express = require("express");
+var file = "test.db";
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(file);
 var app = express();
-var passport = require('passport')
-  , FacebookStrategy = require('passport-facebook').Strategy;
+var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy;
+
+app.configure(function() {
+  app.use(express.static('public'));
+  app.use(express.cookieParser());
+  app.use(express.bodyParser());
+  app.use(express.session({ secret: 'keyboard cat' }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  app.use(app.router);
+});
+
+//blame tim for this; creates fb user entry table
+function createDbTables() {
+	db.run("CREATE TABLE IF NOT EXISTS fbuser\
+		    (id INTEGER,\
+		    fbid INTEGER)");
+}
+createDbTables();
+
+// serialize and deserialize
+passport.serializeUser(function(user, done) {
+done(null, user);
+});
+passport.deserializeUser(function(obj, done) {
+done(null, obj);
+});
 
 passport.use(new FacebookStrategy({
-    clientID: 676552805730069,
-    clientSecret: 5c8d7826b50a1474c42d9d46ce30a4fd,
-    callbackURL: "http://www.dinewithdinex.com/"
+    clientID: '676552805730069',
+    clientSecret: '5c8d7826b50a1474c42d9d46ce30a4fd',
+    callbackURL: "http://localhost:12000/auth/facebook/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     //check user table for anyone with a facebook ID of profile.id
@@ -35,10 +64,36 @@ passport.use(new FacebookStrategy({
             //         //found user. Return
             //         return done(err, user);
             //     }
-            
-            });
-        }
-    ));
+            console.log(profile);
+            //this is a very bad order to do it in. blame tim.
+        	var query_string = "select id from fbuser where fbid = " + profile.id;
+        	db.serialize(function() {
+        		db.get(query_string, function(err, rows) {
+        			if(err) {
+        				console.log(err);
+        			}
+        			if(!rows) { //user is registering
+        				var qs = "insert into user\
+        					    (name)\
+        					    values (" + "\"" + profile.displayName + "\")";
+        				console.log(qs);
+        				db.run(qs);
+        				db.get("select last_insert_rowid()", function(err, rows) {
+        					if(err) {
+        						console.log(err);
+        					}
+        					console.log(rows['last_insert_rowid()']);
+        					var qs = "insert into fbuser\
+        						    values ("+ rows['last_insert_rowid()'] + "," + profile.id + ")";
+        					console.log(qs);
+        					db.run(qs);
+        				});
+        			}
+        		})
+        	});
+            return done(null, profile.displayName);
+            })
+    );
 
 // Redirect the user to Facebook for authentication.  When complete,
 // Facebook will redirect the user back to the application at
@@ -49,16 +104,23 @@ app.get('/auth/facebook', passport.authenticate('facebook'));
 // authentication process by attempting to obtain an access token.  If
 // access was granted, the user will be logged in.  Otherwise,
 // authentication has failed.
-app.get('/auth/facebook/callback', 
-  passport.authenticate('facebook', { successRedirect: '/success',
-                                      failureRedirect: '/login' }));
+app.get('/auth/facebook/callback',
+	passport.authenticate('facebook', {failureRedirect: '/fail', successRedirect: '/success'}),
+	function(req, res) {
+		console.log("great success!");
+ 		res.redirect('/success');
+});
 
 app.get('/', function(req,res) {
 	res.send('<a href="/auth/facebook">Login with Facebook</a>');
 });
 
-app.get('/', function(req,res) {
+app.get('/success', function(req,res) {
 	res.send("you're logged in with facebook!");
+});
+
+app.get('/fail', function(req,res) {
+	res.send("you goddamn failure");
 });
 
 app.listen(12000);
