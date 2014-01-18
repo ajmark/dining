e12f3335b24a19076e9b4eea7b9ce8a702ba966d
Vincent Huang <vincom2@gmail.com>
2014-01-18 04:22:50 -0500

    much amaze such tim

--- server/server.js
+++ server/server.js
@@ -2,24 +2,11 @@ var express = require('express');
 // var fs = require("fs");
 var file = "test.db";
 // var exists = fs.existsSync(file);
-// var sql = require('sql'); //help this doesn't work I am sad
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database(file);
 var app = express();
 
-// var chatlogs = sql.define({
-// 	name: 'chats',
-// 	columns: ['fromID', 'toID', 'msg', 'time']
-// });
-
 app.get('/get_chats', function(req,res) {
-	// var bad_query_string = chatlogs
-	// 					.select(chatlogs.msg, chatlogs.time)
-	// 					.from(chatlogs)
-	// 					.where(chatlogs.fromID.equals(req.query.from))
-	// 					.toQuery();
-	// console.log(bad_query_string.text);
-	// var query = db.prepare("select * from chats where fromID = 1");
 	var query_string = "select * from chats where fromID = " + req.query.from;
 
 	db.serialize(function() {
