52eeb1c33c1571d74d401f46ab763ec34aca3d07
root <root@host1.blitkz.com>
2014-01-18 04:19:35 -0500

    removed http proxy

--- server/server.js
+++ server/server.js
@@ -4,17 +4,6 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database(file);
 var app = express();
 
-var httpProxy = require("http-proxy");
-var options = {
-                hostnameOnly : true,
-                router: {
-                            "www.dinewithdinex.com" : "127.0.0.1:8000",
-                            "dinewithdinex.com" : "127.0.0.1:8000",
-                            "api.dinewithdinex.com" : "127.0.0.1:3000"
-                }
-            }
-
-
 
 app.get('/get_chats', function(req,res) {
 	var query_string = "select * from chats where fromID = " + req.query.from;
@@ -35,5 +24,3 @@ app.get('/', function(req, res){
 });
 
 app.listen(3000);
-var proxyServer = httpProxy.createServer(options);
-proxyServer.listen(80);
