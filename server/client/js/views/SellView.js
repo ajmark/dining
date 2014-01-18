9a5f6ce1451a78c5561ed7c5e159e2a38ad73865
Arthur Lee <arthur@arthurlee.me>
2014-01-18 18:48:51 -0500

    Added many things. So tired.

--- server/client/js/views/SellView.js
+++ server/client/js/views/SellView.js
@@ -2,6 +2,9 @@
   'use strict';
   window.App = window.App || {Collections: {}, Models: {}, Views: {}};
   App.Views.SellView = Backbone.View.extend({
+    events: {
+      'click #foursquare': 'foursquarePopup'
+    },
     initialize: function (options) {
 
     },
@@ -9,6 +12,24 @@
       this.$el.attr('id', 'page-sell');
       this.$el.html(_.template($('#tpl-sell').html(), {}));
       return this;
+    },
+    foursquarePopup: function() {
+      $('.venue-chooser').fadeIn();
+
+      navigator.geolocation.getCurrentPosition(function(position) {
+        $.get('/api/get_coords?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude, function(data) {
+          var ul = $('<ul>');
+
+          for (var i in data) {
+            var a = $('<a>');
+            a.attr('href','#').html(data[i].name + ' <br>' + data[i].addr);
+            ul.append($('<li>').append(a));
+          }
+
+          $('.venues').append(ul);
+          
+        });
+      });
     }
   });
 })();
