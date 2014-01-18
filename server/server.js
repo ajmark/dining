add93f86ee9ef34b35051dafd568d74a7c357cb2
Arthur Lee <arthur@arthurlee.me>
2014-01-18 11:52:50 -0500

    Removed redundant middleware, removed CORS because we are now serving static files from the same server

--- server/server.js
+++ server/server.js
@@ -139,8 +139,7 @@ function createDbTables(){
 
 createDbTables();
 
-app.use(express.json());
-app.use(cors());
+//app.use(cors());
 
 app.post("/api/add_listing", function(req, res){
 	console.log(req.body);
