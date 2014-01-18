5da2c779bab2a55ee46d26f5f6082a6e237894a3
Joel Choo <joelchoo@gmail.com>
2014-01-18 07:22:46 -0500

    Made changes

--- server/server.js
+++ server/server.js
@@ -20,15 +20,14 @@ function createDbTables(){
 
 createDbTables();
 
+app.use(express.json());
+
 // To get chats between 2 people
 app.get('/get_chats', function(req,res) {
-  console.log(req.query.lastID);
-	var query_string = "select * from chats where ((fromID = " 
-  + req.query.from + " and toID = " + req.query.to
-  + ") or (fromID = " + req.query.to + " and toID = " + req.query.from + "))"
-  + " and (id>" + req.query.lastID + ")";
-
-  var query_string2 = "select * from chats";
+	var query_string = "SELECT * FROM chats WHERE ((fromID = " 
+  + req.query.from + " AND toID = " + req.query.to
+  + ") OR (fromID = " + req.query.to + " AND toID = " + req.query.from + "))"
+  + " AND (id>" + req.query.lastID + ")";
 
 	db.serialize(function() {
 		db.all(query_string, function(err, rows) {
@@ -38,6 +37,47 @@ app.get('/get_chats', function(req,res) {
 			res.send(rows);
 		});
 	});
+});
+
+// To get all chats for debugging purposes
+app.get('/get_all_chats', function(req,res) {
+  var query_string = "SELECT * FROM chats ";
+
+  db.serialize(function() {
+    db.all(query_string, function(err, rows) {
+      if(err) {
+        console.log(err);
+      }
+      res.send(rows);
+    });
+  });
+});
+
+
+
+// To update chat when a user sends a message
+app.post('/send_message', function(req, res){
+  console.log("test");
+  // req.body.whatever
+  var query_string2 = "INSERT INTO chats (fromID, toID, msg) values (" 
+    + req.body.from + ", " + req.body.to + ", " + req.body.msg + ")";
+
+  console.log(req.body.msg);
+
+  
+  var query_string = "INSERT INTO chats(msg) VALUES(" + req.body.msg + ")";
+
+
+  db.serialize(function(){
+    // console.log("1");
+    db.all(query_string, function(err, rows) {
+      if (err){
+        console.log(err);
+      }
+      // console.log("2");
+      res.send(rows);
+    });
+  });
 });
 
 var http = require('http');
