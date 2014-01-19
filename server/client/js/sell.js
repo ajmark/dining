abd8fe2a5263018f1e8c6ad73b90bad2a98f8d51
Timothy Koh <k.o.htimothy@gmail.com>
2014-01-19 03:07:31 -0500

    upadted sell form to extract lat lng location data

--- server/client/js/sell.js
+++ server/client/js/sell.js
@@ -2,11 +2,14 @@ $(window).load(function(){
 	$("#foursquare").click(function(){
 		navigator.geolocation.getCurrentPosition(function(position) {
 	        $.get('/api/get_coords?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude, function(data) {
+	        	console.log(data);
 	          var ul = $('<ul>');
 
 	          for (var i in data) {
 	            var a = $('<a>');
 	            a.attr('href','#').html(data[i].name + ' <br>' + data[i].addr);
+	            a.data("lat", data[i].lat);
+	            a.data("lng", data[i].lng);
 	            ul.append($('<li>').append(a));
 	          }
 
@@ -25,6 +28,8 @@ $(window).load(function(){
 
 	$(".venues").on("click", "a", function(){
 		$("#foursquare").html($(this).html());
+		$("#foursquare").data("lat", $(this).data("lat"));
+		$("#foursquare").data("lng", $(this).data("lng"));
 		$(".venue-chooser").fadeOut();
 	});
       
